d7dc8b3cc9135666d2511b03b10a91fdbee957be
Graystevo <grayrules@yahoo.com>
2025-01-10 20:55:59 -0700

    Made prep 2 grade letter program.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int gradePercent = int.Parse(userInput);
+        string gradeLetter = "";
+        bool passClass = false;
+
+        if (gradePercent >= 90)
+        {
+            gradeLetter = "A";
+            passClass = true;
+        }
+        else if (gradePercent >= 80)
+        {
+            gradeLetter = "B";
+            passClass = true;
+        }
+        else if (gradePercent >= 70)
+        {
+            gradeLetter = "C";
+            passClass = true;
+        }
+        else if (gradePercent >= 60)
+        {
+            gradeLetter = "D";
+            passClass = false;
+        }
+        else
+        {
+            gradeLetter = "F";
+            passClass = false;
+        }
+
+        Console.WriteLine($"You got {gradeLetter} as your final grade.");
+
+        if (passClass == true)
+        {
+            Console.WriteLine("Good job on passing!");
+        }
+        else
+        {
+            Console.WriteLine("You suck.");
+        }
     }
 }
