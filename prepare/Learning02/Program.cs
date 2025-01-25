a9f0f85f1ba2fa11567214e1c714b253975713ef
Graystevo <grayrules@yahoo.com>
2025-01-24 18:35:33 -0700

    added notes, made public class, did w3 learning activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 1911;
+        job1._endYear = 1955;
+        
+        // Console.WriteLine("job1 created");
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software tester";
+        job2._company = "Apple";
+        job2._startYear = 1911;
+        job2._endYear = 1955;
+
+        // Console.WriteLine("job2 created");
+
+        // replace with Display call.
+        Resume resume1 = new Resume();
+        resume1._name = "John test";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Resume
+{
+    // persons name
+    public string _name;
+
+    // Member variable for the list of jobs
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        // Notice the use of the custom data type "Job" in this loop
+        foreach (Job job in _jobs)
+        {
+            // This calls the Display method on each job
+            job.DisplayJobDetails();
+        }
+    }
+}
