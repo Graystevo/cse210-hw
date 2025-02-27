8d80382b2f712b354f49aa48bd7b3cfd274c900b
Graystevo <grayrules@yahoo.com>
2025-02-26 18:29:52 -0700

    added note to program.cs

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -11,3 +11,6 @@ namespace MindfulnessApp
         }
     }
 }
+
+// the creative aspect of this program is the implementation of a menu class that can be used in any other program very easily.
+// I also made the wait animation method a variable class that can receive input for how long the animation should play. 
